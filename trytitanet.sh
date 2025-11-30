3706ceab29fba851309ef205dea3bddb79c16a85
Paul Irish <commits@paul.irish>
2025-11-30 12:20:42 -0800

    put py and testfiles under git

---
+++ trytitanet.sh
@@ -0,0 +1,5 @@
+set -euxo pipefail
+
+uv run --native-tls --project data/whisperx-env/parakeet/ python data/whisperx-env/parakeet/titanet_identify.py data/whisperx-env/parakeet/test_files/mono-boobs.wav data/whisperx-env/parakeet/test_files/boobs.input_segments.json boobs.output_segments.json --qdrant qdrant --collection speakers --threshold 0.5
+
+
